1dcc7ac7e1684e635acd1a245433a2e0f6490e73
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-09-05 11:24:07 -0300

    Configurando o Router

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,26 +1,14 @@
-import Title from './components/Title'
-import Counter from './components/Counter'
+import { Outlet } from 'react-router-dom'
 
 import WallPaper from './assets/wallpaper.jpg'
 
-import useCountdown from './hook/useCountdown'
-
-
 import './App.css'
 
-function App() {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2026 00:00:00")
-
+function App() { 
   return (
     <div className='App' style={{ backgroundImage: `url(${WallPaper})` }}>
       <div className="container">
-        <Title title="Contagem regressiva para 2026" />
-        <div className="countdown-container">
-          <Counter title="Dias" number={day} />
-          <Counter title="Horas" number={hour} />
-          <Counter title="Minutos" number={minute} />
-          <Counter title="Segundos" number={second} />
-        </div>
+        <Outlet />
       </div>
     </div>
   )
